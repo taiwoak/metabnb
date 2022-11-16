858a87612662584bc3903c37f1fc214eac25999b
Taiwo Akerele <taiwoakerele98@gmail.com>
2022-11-16 20:51:40 +0100

    build deployment files

---
+++ precache-manifest.c1d0d485506ab91c1ed4c9e0e053a453.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b50a4be98a8712f95abe",
+    "url": "/metabnb/static/css/main.7509640c.chunk.css"
+  },
+  {
+    "revision": "b50a4be98a8712f95abe",
+    "url": "/metabnb/static/js/main.b50a4be9.chunk.js"
+  },
+  {
+    "revision": "21453aad526ab01ba1d8",
+    "url": "/metabnb/static/css/1.271ca620.chunk.css"
+  },
+  {
+    "revision": "21453aad526ab01ba1d8",
+    "url": "/metabnb/static/js/1.21453aad.chunk.js"
+  },
+  {
+    "revision": "f1d6838ce1c6b5e86549",
+    "url": "/metabnb/static/js/runtime~main.f1d6838c.js"
+  },
+  {
+    "revision": "48539b0a0ed87eb2c380b3875f2e4c36",
+    "url": "/metabnb/index.html"
+  }
+];
